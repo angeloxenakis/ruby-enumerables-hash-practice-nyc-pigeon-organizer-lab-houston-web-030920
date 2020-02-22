3e99af91eea9ff648aa1b92c778c88282dbbab6f
Angelo Xenakis <angeloxenakis@gmail.com>
2020-02-22 04:04:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require 'pry'
+require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data[:color].each do |color, names_array| 
+    names_array.map do |name|
+      if !pigeon_list.include? name
+        pigeon_list[name] = {
+          :color => [],
+          :gender => [],
+          :lives => []
+        }
+      end 
+    end 
+  end
+  data.each do |trait_type, trait_data|
+    trait_type.each do |specific_trait, names_array|
+      names_array.map do |name|
+        pigeon_list[name][trait_type].push(trait.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
+
+
+  
